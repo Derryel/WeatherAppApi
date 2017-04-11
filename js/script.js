d5282390b4e3f0e23647ad7e187eed85adecfd1b
Derryel Anderson <derryelanderson@gmail.com>
2017-04-11 19:28:17 -0400

    MVP

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
 			});
 
 		}else{
-			$("#error").html('Field cannot be empty');
+			$("#error").html("<div class='alert alert-danger'><a herf='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>Field cannot be empty</div>");
 		}
 		
 	});
@@ -26,12 +26,12 @@ $(document).ready(function() {
 function show(data) {
 	return "<h2><strong>Current Weather for</strong>: "+ data.name + ", "+ data.sys.country +"</h2>" +
 		   "<h3><strong>Weather</strong>: "+data.weather[0].main+"</h3>" +
-		   "<h3><strong>Description</strong>: "+data.weather[0].description+"</h3>" +
-		   "<h3><strong>Temperature</strong>: "+data.main.temp+"</h3>" +
-		   "<h3><strong>High</strong>: "+data.main.temp_max+"</h3>" +
-		   "<h3><strong>Low</strong>: "+data.main.temp_min+"</h3>" +
-		   "<h3><strong>humidity</strong>: "+data.main.humidity+"</h3>" +
-		   "<h3><strong>Wind Speed</strong>: "+data.wind.speed+"</h3>" +
+		   "<h3><strong>Description</strong>:<img src='http://openweathermap.org/img/w/"+data.weather[0].icon+".png'> "+data.weather[0].description+"</h3>" +
+		   "<h3><strong>Temperature</strong>: "+data.main.temp+"&deg;F</h3>" +
+		   "<h3><strong>High</strong>: "+data.main.temp_max+"&deg;F</h3>" +
+		   "<h3><strong>Low</strong>: "+data.main.temp_min+"&deg;F</h3>" +
+		   "<h3><strong>humidity</strong>: "+data.main.humidity+"%</h3>" +
+		   "<h3><strong>Wind Speed</strong>: "+data.wind.speed+" m/s</h3>" +
 		   "<h3><strong>Sunrise</strong>: "+data.sys.sunrise+"</h3>" +
 		   "<h3><strong>Sunset</strong>: "+data.sys.sunset+"</h3>";
 }
